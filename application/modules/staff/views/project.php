601da0a92d40cdee815d849651bfca5797dfec5e
TopEpp <natchapol.prms@gmail.com>
2018-06-16 02:55:14 +0700

    edit

--- application/modules/staff/views/project.php
+++ application/modules/staff/views/project.php
@@ -119,7 +119,7 @@
                           <div class="card-block">
 
                             <div class="wysiwyg5-wrapper b-a b-grey">
-                              <textarea required id="project_detail" name="project_detail" class="wysiwyg demo-form-wysiwyg project_detail" placeholder="โปรดระบุบ เกี่ยวกับกิจกรรม..." ui-jq="wysihtml5" ui-options="{
+                              <textarea required id="project_detail" name="project_detail" class=" demo-form-wysiwyg project_detail" placeholder="โปรดระบุบ เกี่ยวกับกิจกรรม..." ui-jq="wysihtml5" ui-options="{
                               html: true,
                               stylesheets: ['pages/css/editor.css']
                             }"><?php if(!empty($prj)){ echo $prj->project_detail;}?></textarea>
@@ -134,7 +134,7 @@
                           <div class="card-block">
 
                             <div class="wysiwyg5-wrapper b-a b-grey">
-                              <textarea required id="project_provenance" name="project_provenance" class="wysiwyg demo-form-wysiwyg project_provenance" placeholder="โปรดระบุบเงื่อนไขและข้อตกลง ..." ui-jq="wysihtml5" ui-options="{
+                              <textarea required id="project_provenance" name="project_provenance" class=" demo-form-wysiwyg project_provenance" placeholder="โปรดระบุบเงื่อนไขและข้อตกลง ..." ui-jq="wysihtml5" ui-options="{
                               html: true,
                               stylesheets: ['pages/css/editor.css']
                             }"><?php if(!empty($prj)){ echo $prj->project_provenance;}?></textarea>
