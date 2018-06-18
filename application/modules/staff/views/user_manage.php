c5c7933a70c19ac17c9fc346a336784d176c98d9
TopEpp <natchapol.prms@gmail.com>
2018-06-18 22:58:25 +0700

    edit form user

--- application/modules/staff/views/user_manage.php
+++ application/modules/staff/views/user_manage.php
@@ -6,7 +6,7 @@
               <div class="inner">
                 <!-- START BREADCRUMB -->
                 <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="#">หน้าหลัก</a></li>
+                  <li class="breadcrumb-item"><a href="#">หน้าแรก</a></li>
                   <li class="breadcrumb-item active">ผู้ใช้งาน</li>
                 </ol>
                 <!-- END BREADCRUMB -->
