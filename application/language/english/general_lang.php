fc3f49f8a33bc44ec6bc36ac1f28dc7197720ccc
TopEpp <natchapol.prms@gmail.com>
2018-09-16 21:10:42 +0700

    add lang en

--- application/language/english/general_lang.php
+++ application/language/english/general_lang.php
@@ -15,3 +15,141 @@ $lang['user'] = 'User';
 $lang['help'] = 'Help';
 $lang['news'] = 'News';
 $lang['history'] = 'History';
+$lang['Homepage'] = 'Homepage';
+$lang['Profile'] = 'Profile';
+
+#top menu
+$lang['Settings'] = 'Settings';
+$lang['Sign out'] = 'Sign out';
+
+#home
+$lang['Account'] = 'Chiang Mai Design Week Account';
+$lang['Email'] = 'Email';
+$lang['Required'] = 'Required information';
+$lang['Password'] = 'Password';
+$lang['Remember'] = 'Remember my password';
+$lang['SignIn'] = 'Sign in to your account';
+$lang['Create_account'] = 'Create account';
+$lang['Forget_password'] = 'Forget password';
+$lang['Report_login'] = 'Report a login or sign up issue';
+$lang['Confirm'] = 'Confirm';
+$lang['Cancel'] = 'Cancel';
+$lang['Contact_email'] = 'Contact email address';
+$lang['Description_issue'] = 'Description of the issue';
+
+#create account
+$lang['Create_User'] = 'Create a User Account';
+$lang['Confirm_Email'] = 'Confirm Email';
+$lang['Confirm_Password'] = 'Confirm Password';
+$lang['User_Information'] = 'User Information';
+$lang['Title'] = 'Title';
+$lang['Select'] = 'Select';
+$lang['First_Name'] = 'First Name';
+$lang['Last_Name'] = 'Last Name';
+$lang['Birth_Date'] = 'Birth Date';
+$lang['Birth_Month'] = 'Birth Month';
+$lang['Birth_Year'] = 'Birth Year';
+$lang['Contact_Address'] = 'Contact Address';
+$lang['Name_Company';
+$lang['House_No'] = 'House No.';
+$lang['Moo'] = 'Moo';
+$lang['Soi'] = 'Soi';
+$lang['Road'] = 'Road';
+$lang['Subdistrict'] = 'Subdistrict';
+$lang['District/Area'] = 'District/Area';
+$lang['Country'] = 'Country';
+$lang['Province'] = 'Province';
+$lang['Postcode'] = 'Postcode';
+$lang['Profile_Image'] = 'Profile Image';
+$lang['I_have_read'] = 'I have read and agree to the terms and conditions';
+
+#News
+$lang['News'] = 'News';
+$lang['Notification'] = 'Notification';
+$lang['Passed'] = 'Passed';
+$lang['Date'] = 'Date';
+$lang['Proceed'] = 'Proceed';
+$lang['Send_additional'] = 'Send additional documents';
+
+#Home User
+$lang['Open_Application'] = 'Open for Application';
+$lang['Activity_Name'] = 'Activity Name';
+$lang['Category'] = 'Category';
+$lang['Status'] = 'Status';
+$lang['Latest_Update'] = 'Latest Update';
+$lang['Applied'] = 'Applied';
+$lang['Details'] = 'Details';
+$lang['Application_Period'] = 'Application Period';
+$lang['Activity_Period'] = 'Activity Period';
+$lang['Apply'] = 'Apply';
+$lang['Click_apply'] = 'Click to apply';
+
+#Profile
+$lang['Waiting_confirmation'] = 'Waiting for confirmation';
+$lang['View'] = 'View';
+$lang['Activity_Details'] = 'Activity Details';
+
+#Edit User Account
+$lang['Edit_User_Account'] = 'Edit User Account';
+$lang['Edit_Information'] = 'Edit User Information';
+$lang['Edit_Account'] = 'Edit Account';
+$lang['First_name-Last_name'] = 'First name-Last name';
+$lang['Personal_Information'] = 'Personal Information';
+$lang['Phone_No'] = 'Phone No.';
+$lang['Note_jpg'] = 'Note: .jpg or .png file up to 2 MB';
+
+$lang['Account_Status'] = 'Account Status';
+$lang['Change_Account_Status'] = 'Change Account Status';
+$lang['Account_Information'] = 'Account Information';
+$lang['Old_Password'] = 'Old Password';
+$lang['Enter_your_old_password'] = 'Enter your old password';
+$lang['New_Password'] = 'New Password';
+$lang['Enter_your_new_password'] = 'Enter your new password of at least 8 characters';
+$lang['Confirm_New_Password'] = 'Confirm New Password';
+$lang['Enter_your_new_password_again'] = 'Enter your new password again';
+$lang['Choose_a_password'] = 'Choose a password of at least 8 characters containing letters, numbers and symbols';
+$lang['Mr.'] = 'Mr.';
+$lang['Mrs.'] = 'Mrs.Ms.';
+$lang['Ms.'] = 'Ms.';
+$lang['Not_Specify'] = 'Not Specify';
+
+#Activity
+$lang['Terms_Conditions'] = 'Terms and Conditions';
+$lang['Personal/Organisational_Information'] = 'Personal/Organisational Information';
+$lang['Design_Work'] = 'Design Work';
+$lang['Display_Method'] = 'Display Method';
+$lang['Evaluation_Form'] = 'Evaluation Form';
+$lang['Terms_Conditions_Participating'] = 'Terms and Conditions for Participating in Showcase or Exhibition';
+$lang['1_Interested'] = '1.	Interested individuals can complete the online application form from June 20 – July 15, 2018.';
+$lang['2_The_work'] = '2.	The work submitted for display must not copy or duplicate the work of others. In the event of a dispute or claim of infringement of copyright or any other intellectual properties in connection with the displayed work, the participant must assume sole responsibility for payment of compensation for damages resulting from the infringement of copyright or any other rights.';
+$lang['3_articipant'] = '3.	Participant must study the criteria for participating in the activity and follow all of the criteria. Participant must also agree to the disclaimer issued by the Chiang Mai Design Week to cancel, amend, alter or change the terms and conditions as the organiser of Chiang Mai Design Week sees fit without prior notice. At all events, the decision of the organiser of Chiang Mai Design Week shall be final.';
+$lang['4_Application'] = '4.	Application information and documents must be accurate and truthful. In the event that the application information and documents are later found to be inaccurate or false, the participant will be immediately considered lacking in qualifications. And in the case that the participant’s inaccurate or false information causes any damages to the Festival, the participant will accept unconditional responsibility.';
+
+$lang['Address_Sending'] = 'Address for Sending Documents';
+$lang['Company/Building_Name'] = 'Company/Building Name';
+$lang['Mobile_Phone'] = 'Mobile Phone';
+
+$lang['In_which_status_do_you_apply'] = 'In which status do you apply?';
+$lang['Which_creative_industry_does_your_work_belong_to'] = 'Which creative industry does your work belong to?';
+
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
